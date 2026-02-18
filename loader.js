2998650a18d17115ca5359aefe964af1ef497c62
luacoder312 <harshisop3@gmail.com>
2026-02-18 22:23:02 +0530

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,6 +1,7 @@
 export default async function handler(req, res) {
   const response = await fetch("https://raw.githubusercontent.com/luacoder312/Script-loader/main/script.lua");
   const text = await response.text();
+
   res.setHeader("Content-Type", "text/plain");
   res.status(200).send(text);
 }
